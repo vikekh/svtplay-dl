0d59e38bafb64910fcb67fd877650768b3c8a99b
Johan Andersson <j@i19.se>
2018-01-14 00:04:07 +0100

    get_one_media: remove some stdout code

--- lib/svtplay_dl/__init__.py
+++ lib/svtplay_dl/__init__.py
@@ -309,7 +309,7 @@ def get_one_media(stream, options):
         elif options.merge_subtitle:
             options.merge_subtitle = False
 
-    if options.subtitle and options.output != "-" and not options.get_url:
+    if options.subtitle and not options.get_url:
         options_subs_dl(subfixes)
         if options.force_subtitle:
             return
@@ -340,11 +340,7 @@ def get_one_media(stream, options):
             sys.exit(2)
 
         if options.thumbnail and hasattr(stream, "get_thumbnail"):
-            if options.output != "-":
-                log.info("Getting thumbnail")
-                stream.get_thumbnail(options)
-            else:
-                log.warning("Can not get thumbnail when fetching to stdout")
+            stream.get_thumbnail(options)
         post = postprocess(stream, options, subfixes)
         if stream.name() == "dash" and post.detect:
             post.merge()
